ec53bcf09939cafbdc0c7ac74533a5470e38fb18
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-03-04 14:57:26 +0600

    project uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,656 @@
+// alert('ok')
+let color1 = document.querySelector('.grid1').style.backgroundColor;
+let color2 = document.querySelector('.grid2').style.backgroundColor;
+let color3 = document.querySelector('.grid3').style.backgroundColor;
+let color4 = document.querySelector('.grid4').style.backgroundColor;
+let color5 = document.querySelector('.grid5').style.backgroundColor;
+let color6 = document.querySelector('.grid6').style.backgroundColor;
+let color7 = document.querySelector('.grid7').style.backgroundColor;
+let color8 = document.querySelector('.grid8').style.backgroundColor;
+let color9 = document.querySelector('.grid9').style.backgroundColor;
+let color10 = document.querySelector('.grid10').style.backgroundColor;
+let color11 = document.querySelector('.grid11').style.backgroundColor;
+let color12 = document.querySelector('.grid12').style.backgroundColor;
+let color13 = document.querySelector('.grid13').style.backgroundColor;
+let color14 = document.querySelector('.grid14').style.backgroundColor;
+let color15 = document.querySelector('.grid15').style.backgroundColor;
+let color16 = document.querySelector('.grid16').style.backgroundColor;
+let color17 = document.querySelector('.grid17').style.backgroundColor;
+let color18 = document.querySelector('.grid18').style.backgroundColor;
+let color19 = document.querySelector('.grid19').style.backgroundColor;
+let color20 = document.querySelector('.grid20').style.backgroundColor;
+let color21 = document.querySelector('.grid21').style.backgroundColor;
+let color22 = document.querySelector('.grid22').style.backgroundColor;
+let color23 = document.querySelector('.grid23').style.backgroundColor;
+let color24 = document.querySelector('.grid24').style.backgroundColor;
+let color25 = document.querySelector('.grid25').style.backgroundColor;
+let color26 = document.querySelector('.grid26').style.backgroundColor;
+let color27 = document.querySelector('.grid27').style.backgroundColor;
+let color28 = document.querySelector('.grid28').style.backgroundColor;
+let color29 = document.querySelector('.grid29').style.backgroundColor;
+let color30 = document.querySelector('.grid30').style.backgroundColor;
+let color31 = document.querySelector('.grid31').style.backgroundColor;
+let color32 = document.querySelector('.grid32').style.backgroundColor;
+let color33 = document.querySelector('.grid33').style.backgroundColor;
+let color34 = document.querySelector('.grid34').style.backgroundColor;
+let color35 = document.querySelector('.grid35').style.backgroundColor;
+let color36 = document.querySelector('.grid36').style.backgroundColor;
+let color37 = document.querySelector('.grid37').style.backgroundColor;
+let color38 = document.querySelector('.grid38').style.backgroundColor;
+let color39 = document.querySelector('.grid39').style.backgroundColor;
+let color40 = document.querySelector('.grid40').style.backgroundColor;
+
+let myBtn1 = document.querySelector('.myBtn1');
+let myBtn2 = document.querySelector('.myBtn2');
+let myBtn3 = document.querySelector('.myBtn3');
+let myBtn4 = document.querySelector('.myBtn4');
+let myBtn5 = document.querySelector('.myBtn5');
+let myBtn6 = document.querySelector('.myBtn6');
+let myBtn7 = document.querySelector('.myBtn7');
+let myBtn8 = document.querySelector('.myBtn8');
+let myBtn9 = document.querySelector('.myBtn9');
+let myBtn10 = document.querySelector('.myBtn10');
+let myBtn11 = document.querySelector('.myBtn11');
+let myBtn12 = document.querySelector('.myBtn12');
+let myBtn13 = document.querySelector('.myBtn13');
+let myBtn14 = document.querySelector('.myBtn14');
+let myBtn15 = document.querySelector('.myBtn15');
+let myBtn16 = document.querySelector('.myBtn16');
+let myBtn17 = document.querySelector('.myBtn17');
+let myBtn18 = document.querySelector('.myBtn18');
+let myBtn19 = document.querySelector('.myBtn19');
+let myBtn20 = document.querySelector('.myBtn20');
+let myBtn21 = document.querySelector('.myBtn21');
+let myBtn22 = document.querySelector('.myBtn22');
+let myBtn23 = document.querySelector('.myBtn23');
+let myBtn24 = document.querySelector('.myBtn24');
+let myBtn25 = document.querySelector('.myBtn25');
+let myBtn26 = document.querySelector('.myBtn26');
+let myBtn27 = document.querySelector('.myBtn27');
+let myBtn28 = document.querySelector('.myBtn28');
+let myBtn29 = document.querySelector('.myBtn29');
+let myBtn30 = document.querySelector('.myBtn30');
+let myBtn31 = document.querySelector('.myBtn31');
+let myBtn32 = document.querySelector('.myBtn32');
+let myBtn33 = document.querySelector('.myBtn33');
+let myBtn34 = document.querySelector('.myBtn34');
+let myBtn35 = document.querySelector('.myBtn35');
+let myBtn36 = document.querySelector('.myBtn36');
+let myBtn37 = document.querySelector('.myBtn37');
+let myBtn38 = document.querySelector('.myBtn38');
+let myBtn39 = document.querySelector('.myBtn39');
+let myBtn40 = document.querySelector('.myBtn40');
+
+myBtn1.addEventListener('click', ()=>{
+
+    let copyColor1 = document.createElement('input');
+    copyColor1.setAttribute('value', color1);
+    document.body.appendChild(copyColor1);
+    copyColor1.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor1);
+    toastr.success('Copied:' + copyColor1.value);
+})
+
+
+myBtn2.addEventListener('click', ()=>{
+
+    let copyColor2 = document.createElement('input');
+    copyColor2.setAttribute('value', color2);
+    document.body.appendChild(copyColor2);
+    copyColor2.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor2);
+    toastr.success('Copied:' + copyColor2.value);
+})
+
+
+myBtn3.addEventListener('click', ()=>{
+
+    let copyColor3 = document.createElement('input');
+    copyColor3.setAttribute('value', color3);
+    document.body.appendChild(copyColor3);
+    copyColor3.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor3);
+    toastr.success('Copied:' + copyColor3.value);
+})
+
+
+myBtn4.addEventListener('click', ()=>{
+
+    let copyColor4 = document.createElement('input');
+    copyColor4.setAttribute('value', color4);
+    document.body.appendChild(copyColor4);
+    copyColor4.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor4);
+    toastr.success('Copied:' + copyColor4.value);
+})
+
+
+myBtn5.addEventListener('click', ()=>{
+
+    let copyColor5 = document.createElement('input');
+    copyColor5.setAttribute('value', color5);
+    document.body.appendChild(copyColor5);
+    copyColor5.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor5);
+    toastr.success('Copied:' + copyColor5.value);
+})
+
+
+myBtn6.addEventListener('click', ()=>{
+
+    let copyColor6 = document.createElement('input');
+    copyColor6.setAttribute('value', color6);
+    document.body.appendChild(copyColor6);
+    copyColor6.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor6);
+    toastr.success('Copied:' + copyColor6.value);
+})
+
+
+myBtn7.addEventListener('click', ()=>{
+
+    let copyColor7 = document.createElement('input');
+    copyColor7.setAttribute('value', color7);
+    document.body.appendChild(copyColor7);
+    copyColor7.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor7);
+    toastr.success('Copied:' + copyColor7.value);
+})
+
+
+myBtn8.addEventListener('click', ()=>{
+
+    let copyColor8 = document.createElement('input');
+    copyColor8.setAttribute('value', color8);
+    document.body.appendChild(copyColor8);
+    copyColor8.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor8);
+    toastr.success('Copied:' + copyColor8.value);
+})
+
+
+myBtn9.addEventListener('click', ()=>{
+
+    let copyColor9 = document.createElement('input');
+    copyColor9.setAttribute('value', color9);
+    document.body.appendChild(copyColor9);
+    copyColor9.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor9);
+    toastr.success('Copied:' + copyColor9.value);
+})
+
+
+myBtn10.addEventListener('click', ()=>{
+
+    let copyColor10 = document.createElement('input');
+    copyColor10.setAttribute('value', color10);
+    document.body.appendChild(copyColor10);
+    copyColor10.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor10);
+    toastr.success('Copied:' + copyColor10.value);
+})
+
+
+myBtn11.addEventListener('click', ()=>{
+
+    let copyColor11 = document.createElement('input');
+    copyColor11.setAttribute('value', color11);
+    document.body.appendChild(copyColor11);
+    copyColor11.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor11);
+    toastr.success('Copied:' + copyColor11.value);
+})
+
+
+myBtn12.addEventListener('click', ()=>{
+
+    let copyColor12 = document.createElement('input');
+    copyColor12.setAttribute('value', color12);
+    document.body.appendChild(copyColor12);
+    copyColor12.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor12);
+    toastr.success('Copied:' + copyColor12.value);
+})
+
+
+myBtn13.addEventListener('click', ()=>{
+
+    let copyColor13 = document.createElement('input');
+    copyColor13.setAttribute('value', color13);
+    document.body.appendChild(copyColor13);
+    copyColor13.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor13);
+    toastr.success('Copied:' + copyColor13.value);
+})
+
+
+myBtn14.addEventListener('click', ()=>{
+
+    let copyColor14 = document.createElement('input');
+    copyColor14.setAttribute('value', color14);
+    document.body.appendChild(copyColor14);
+    copyColor14.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor14);
+    toastr.success('Copied:' + copyColor14.value);
+})
+
+
+myBtn15.addEventListener('click', ()=>{
+
+    let copyColor15 = document.createElement('input');
+    copyColor15.setAttribute('value', color15);
+    document.body.appendChild(copyColor15);
+    copyColor15.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor15);
+    toastr.success('Copied:' + copyColor15.value);
+})
+
+
+myBtn16.addEventListener('click', ()=>{
+
+    let copyColor16 = document.createElement('input');
+    copyColor16.setAttribute('value', color16);
+    document.body.appendChild(copyColor16);
+    copyColor16.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor16);
+    toastr.success('Copied:' + copyColor16.value);
+})
+
+
+myBtn17.addEventListener('click', ()=>{
+
+    let copyColor17 = document.createElement('input');
+    copyColor17.setAttribute('value', color17);
+    document.body.appendChild(copyColor17);
+    copyColor17.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor17);
+    toastr.success('Copied:' + copyColor17.value);
+})
+
+
+myBtn18.addEventListener('click', ()=>{
+
+    let copyColor18 = document.createElement('input');
+    copyColor18.setAttribute('value', color18);
+    document.body.appendChild(copyColor18);
+    copyColor18.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor18);
+    toastr.success('Copied:' + copyColor18.value);
+})
+
+
+myBtn19.addEventListener('click', ()=>{
+
+    let copyColor19 = document.createElement('input');
+    copyColor19.setAttribute('value', color19);
+    document.body.appendChild(copyColor19);
+    copyColor19.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor19);
+    toastr.success('Copied:' + copyColor19.value);
+})
+
+
+myBtn20.addEventListener('click', ()=>{
+
+    let copyColor20 = document.createElement('input');
+    copyColor20.setAttribute('value', color20);
+    document.body.appendChild(copyColor20);
+    copyColor20.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor20);
+    toastr.success('Copied:' + copyColor20.value);
+})
+
+
+myBtn21.addEventListener('click', ()=>{
+
+    let copyColor21 = document.createElement('input');
+    copyColor21.setAttribute('value', color21);
+    document.body.appendChild(copyColor21);
+    copyColor21.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor21);
+    toastr.success('Copied:' + copyColor21.value);
+})
+
+
+myBtn22.addEventListener('click', ()=>{
+
+    let copyColor22 = document.createElement('input');
+    copyColor22.setAttribute('value', color22);
+    document.body.appendChild(copyColor22);
+    copyColor22.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor22);
+    toastr.success('Copied:' + copyColor22.value);
+})
+
+
+myBtn23.addEventListener('click', ()=>{
+
+    let copyColor23 = document.createElement('input');
+    copyColor23.setAttribute('value', color23);
+    document.body.appendChild(copyColor23);
+    copyColor23.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor23);
+    toastr.success('Copied:' + copyColor23.value);
+})
+
+
+myBtn24.addEventListener('click', ()=>{
+
+    let copyColor24 = document.createElement('input');
+    copyColor24.setAttribute('value', color24);
+    document.body.appendChild(copyColor24);
+    copyColor24.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor24);
+    toastr.success('Copied:' + copyColor24.value);
+})
+
+
+myBtn25.addEventListener('click', ()=>{
+
+    let copyColor25 = document.createElement('input');
+    copyColor25.setAttribute('value', color25);
+    document.body.appendChild(copyColor25);
+    copyColor25.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor25);
+    toastr.success('Copied:' + copyColor25.value);
+})
+
+
+myBtn26.addEventListener('click', ()=>{
+
+    let copyColor26 = document.createElement('input');
+    copyColor26.setAttribute('value', color26);
+    document.body.appendChild(copyColor26);
+    copyColor26.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor26);
+    toastr.success('Copied:' + copyColor26.value);
+})
+
+
+myBtn27.addEventListener('click', ()=>{
+
+    let copyColor27 = document.createElement('input');
+    copyColor27.setAttribute('value', color27);
+    document.body.appendChild(copyColor27);
+    copyColor27.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor27);
+    toastr.success('Copied:' + copyColor27.value);
+})
+
+
+myBtn28.addEventListener('click', ()=>{
+
+    let copyColor28 = document.createElement('input');
+    copyColor28.setAttribute('value', color28);
+    document.body.appendChild(copyColor28);
+    copyColor28.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor28);
+    toastr.success('Copied:' + copyColor28.value);
+})
+
+
+myBtn29.addEventListener('click', ()=>{
+
+    let copyColor29 = document.createElement('input');
+    copyColor29.setAttribute('value', color29);
+    document.body.appendChild(copyColor29);
+    copyColor29.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor29);
+    toastr.success('Copied:' + copyColor29.value);
+})
+
+
+myBtn30.addEventListener('click', ()=>{
+
+    let copyColor30 = document.createElement('input');
+    copyColor30.setAttribute('value', color30);
+    document.body.appendChild(copyColor30);
+    copyColor30.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor30);
+    toastr.success('Copied:' + copyColor30.value);
+})
+
+
+myBtn31.addEventListener('click', ()=>{
+
+    let copyColor31 = document.createElement('input');
+    copyColor31.setAttribute('value', color31);
+    document.body.appendChild(copyColor31);
+    copyColor31.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor31);
+    toastr.success('Copied:' + copyColor31.value);
+})
+
+myBtn32.addEventListener('click', ()=>{
+
+    let copyColor32 = document.createElement('input');
+    copyColor32.setAttribute('value', color32);
+    document.body.appendChild(copyColor32);
+    copyColor32.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor32);
+    toastr.success('Copied:' + copyColor32.value);
+})
+
+
+myBtn33.addEventListener('click', ()=>{
+
+    let copyColor33 = document.createElement('input');
+    copyColor33.setAttribute('value', color33);
+    document.body.appendChild(copyColor33);
+    copyColor33.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor33);
+    toastr.success('Copied:' + copyColor33.value);
+})
+
+
+myBtn34.addEventListener('click', ()=>{
+
+    let copyColor34 = document.createElement('input');
+    copyColor34.setAttribute('value', color34);
+    document.body.appendChild(copyColor34);
+    copyColor34.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor34);
+    toastr.success('Copied:' + copyColor34.value);
+})
+
+
+myBtn35.addEventListener('click', ()=>{
+
+    let copyColor35 = document.createElement('input');
+    copyColor35.setAttribute('value', color35);
+    document.body.appendChild(copyColor35);
+    copyColor35.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor35);
+    toastr.success('Copied:' + copyColor35.value);
+})
+
+
+myBtn36.addEventListener('click', ()=>{
+
+    let copyColor36 = document.createElement('input');
+    copyColor36.setAttribute('value', color36);
+    document.body.appendChild(copyColor36);
+    copyColor36.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor36);
+    toastr.success('Copied:' + copyColor36.value);
+})
+
+
+myBtn37.addEventListener('click', ()=>{
+
+    let copyColor37 = document.createElement('input');
+    copyColor37.setAttribute('value', color37);
+    document.body.appendChild(copyColor37);
+    copyColor37.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor37);
+    toastr.success('Copied:' + copyColor37.value);
+})
+
+
+myBtn38.addEventListener('click', ()=>{
+
+    let copyColor38 = document.createElement('input');
+    copyColor38.setAttribute('value', color38);
+    document.body.appendChild(copyColor38);
+    copyColor38.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor38);
+    toastr.success('Copied:' + copyColor38.value);
+})
+
+
+myBtn39.addEventListener('click', ()=>{
+
+    let copyColor39 = document.createElement('input');
+    copyColor39.setAttribute('value', color39);
+    document.body.appendChild(copyColor39);
+    copyColor39.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor39);
+    toastr.success('Copied:' + copyColor39.value);
+})
+
+
+myBtn40.addEventListener('click', ()=>{
+
+    let copyColor40 = document.createElement('input');
+    copyColor40.setAttribute('value', color40);
+    document.body.appendChild(copyColor40);
+    copyColor40.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(copyColor40);
+    toastr.success('Copied:' + copyColor40.value);
+})
+
+
+
+//
+// let copy = document.querySelector('#copy');
+// copy.addEventListener('click', ()=>{
+//     var text = document.createElement('input');
+//     text.setAttribute('value', document.querySelector('.grid').style.backgroundColor);
+//     document.body.appendChild(text);
+//     text.select();
+//     document.execCommand("copy");
+//
+//     document.body.removeChild(text);
+//     alert("Copied the text: " + text.value);
+// })
+
